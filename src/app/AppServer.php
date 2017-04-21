7d980bea6a71b79d5a55e31da2085cc14ae2316d
suibin <157595075@qq.com>
2017-04-22 01:10:54 +0800

    2017-4-22

---
+++ src/app/AppServer.php
@@ -0,0 +1,43 @@
+<?php
+namespace app;
+
+use Server\Asyn\HttpClient\HttpClientPool;
+use Server\SwooleDistributedServer;
+
+/**
+ * Created by PhpStorm.
+ * User: zhangjincheng
+ * Date: 16-9-19
+ * Time: 下午2:36
+ */
+class AppServer extends SwooleDistributedServer
+{
+    /**
+     * 开服初始化(支持协程)
+     * @return mixed
+     */
+    public function onOpenServiceInitialization()
+    {
+        parent::onOpenServiceInitialization();
+    }
+
+    /**
+     * 当一个绑定uid的连接close后的清理
+     * 支持协程
+     * @param $uid
+     */
+    public function onUidCloseClear($uid)
+    {
+        // TODO: Implement onUidCloseClear() method.
+    }
+
+    /**
+     * 这里可以进行额外的异步连接池，比如另一组redis/mysql连接
+     * @return array
+     */
+    public function initAsynPools()
+    {
+        parent::initAsynPools();
+        $this->addAsynPool('DingDingRest', new HttpClientPool($this->config, $this->config->get('dingding.url')));
+    }
+}
